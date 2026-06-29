098d45f0adc44c9a7e212ee39c557e57edc0235c
Pranjal Rai <42915990+pranjalrai-iitd@users.noreply.github.com>
2026-06-29 14:23:28 +0530

    .env updated

--- epic/database/db.js
+++ epic/database/db.js
@@ -1,8 +1,14 @@
 const mysql = require("mysql2");
 
+console.log("DB HOST:", process.env.DB_HOST);
+console.log("DB PORT:", process.env.DB_PORT);
+console.log("DB USER:", process.env.DB_USER);
+console.log("DB NAME:", process.env.DB_NAME);
+
+
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 3306,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
@@ -12,8 +18,8 @@ const connection = mysql.createConnection({
 connection.connect((err) => {
 
     if (err) {
-        console.log("Database connection failed");
-        console.log(err.message);
+        console.log("DATABASE FAILED");
+        console.log(err);
     } else {
         console.log("MySQL connected successfully");
     }
